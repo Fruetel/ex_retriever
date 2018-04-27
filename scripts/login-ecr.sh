62e4b29715b24e2ad3f728a30b868edc388d22bd
Thomas Fruetel <github@fruetel.de>
2018-04-27 23:42:54 +0200

    Add --no-include-email flag

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -5,5 +5,5 @@ pip install awscli --ignore-installed six;
 printf "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY\n" > ~/.aws/credentials;
 printf "[default]\nregion=$AWS_ECR_REGION\n" > ~/.aws/config;
 cat ~/.aws/credentials;
-login_string=`/usr/bin/aws ecr get-login --region eu-west-1`;
+login_string=`/usr/bin/aws ecr get-login --no-include-email`;
 eval $login_string;
