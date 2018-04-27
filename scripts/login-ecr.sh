2828b43beb522fc4cb388f34ab64871b3aa28c0f
Thomas Fruetel <github@fruetel.de>
2018-04-27 23:54:37 +0200

    Do not print credentials any more

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -4,6 +4,5 @@ mkdir ~/.aws;
 pip install awscli --ignore-installed six;
 printf "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY\n" > ~/.aws/credentials;
 printf "[default]\nregion=$AWS_ECR_REGION\n" > ~/.aws/config;
-cat ~/.aws/credentials;
 login_string=`/usr/bin/aws ecr get-login --no-include-email`;
 eval $login_string;
