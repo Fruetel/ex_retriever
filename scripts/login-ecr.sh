0962a981488d8ce437ec6910fd08c032cc7357a3
Thomas Fruetel <github@fruetel.de>
2018-04-27 22:46:22 +0200

    Fix path to aws cli

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -5,5 +5,5 @@ which python
 pip install awscli --ignore-installed six;
 echo "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY" > ~/.aws/credentials;
 echo "[default]\nregion=$AWS_ECR_REGION" > ~/.aws/config;
-login_string=`/usr/local/bin/aws ecr get-login --region eu-west-1`;
+login_string=`/usr/bin/aws ecr get-login --region eu-west-1`;
 eval $login_string;
