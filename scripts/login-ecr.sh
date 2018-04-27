69db88802e0f590ed55d62febdbd36d037678995
Thomas Fruetel <github@fruetel.de>
2018-04-27 23:02:39 +0200

    Check content of credentials file

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "Start logging in ECS"
 mkdir ~/.aws;
-which python
 pip install awscli --ignore-installed six;
 echo "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY" > ~/.aws/credentials;
 echo "[default]\nregion=$AWS_ECR_REGION" > ~/.aws/config;
+cat ~/.aws/credentials;
 login_string=`/usr/bin/aws ecr get-login --region eu-west-1`;
 eval $login_string;
