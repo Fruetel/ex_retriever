68e3c32a56b24e4777f81dc054e67ecc60fbf8a6
Thomas Fruetel <github@fruetel.de>
2018-04-27 23:28:15 +0200

    Add linebreak to config files

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -2,8 +2,8 @@
 echo "Start logging in ECS"
 mkdir ~/.aws;
 pip install awscli --ignore-installed six;
-printf "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY" > ~/.aws/credentials;
-printf "[default]\nregion=$AWS_ECR_REGION" > ~/.aws/config;
+printf "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY\n" > ~/.aws/credentials;
+printf "[default]\nregion=$AWS_ECR_REGION\n" > ~/.aws/config;
 cat ~/.aws/credentials;
 login_string=`/usr/bin/aws ecr get-login --region eu-west-1`;
 eval $login_string;
