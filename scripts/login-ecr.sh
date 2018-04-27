aad2f5ccbb5a104472fe25102fcb646315f9abc7
Thomas Fruetel <github@fruetel.de>
2018-04-27 23:17:15 +0200

    Use printf instead of echo for writing aws configs

--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -2,8 +2,8 @@
 echo "Start logging in ECS"
 mkdir ~/.aws;
 pip install awscli --ignore-installed six;
-echo "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY" > ~/.aws/credentials;
-echo "[default]\nregion=$AWS_ECR_REGION" > ~/.aws/config;
+printf "[default]\naws_access_key_id=$BUILD_ACCESS_KEY\naws_secret_access_key=$BUILD_SECRET_KEY" > ~/.aws/credentials;
+printf "[default]\nregion=$AWS_ECR_REGION" > ~/.aws/config;
 cat ~/.aws/credentials;
 login_string=`/usr/bin/aws ecr get-login --region eu-west-1`;
 eval $login_string;
